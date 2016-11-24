5403befe8523555c58fc1f19e001b91abaa53ceb
Joe Taylor <jt306@sussex.ac.uk>
2016-11-24 16:52:09 +0000

    running on remaining 249

--- run_scripts.R
+++ run_scripts.R
@@ -3,13 +3,11 @@
 #This data corresponds to one of 10 repeats when training 
 #the attribute classifier 'railing', test scenario A.
 
-
+print (commandArgs())
 # print(commandArgs())
 # source("practice.R")
-
-source("simulateGPC.R")
-print (commandArgs())
-# source("simulateGPC_conf2.R", echo=TRUE)
+# source("simulateGPC.R")
+source("simulateGPC_conf2.R", echo=TRUE)
 # source("simulateGPC_plus2.R")
 # source("simulateSVM2.R", echo=TRUE)
 # source("simulateSVMplusQP.R")
